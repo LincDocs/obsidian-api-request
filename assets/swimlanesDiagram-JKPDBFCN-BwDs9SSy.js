57568c89dc8f0914bc99bbc6c35601b2b38a2b4a
LincZero <LincZero@users.noreply.github.com>
2026-09-17 14:50:42 +0000

    Deploying to gh-pages from @ LincDocs/obsidian-api-request@c6b77a28d986ccbf4b4408a24ddbb3a95b7a6b7d 🚀

---
+++ assets/swimlanesDiagram-JKPDBFCN-BwDs9SSy.js
@@ -0,0 +1,8 @@
+import{T as t,f as o}from"./flowDiagram-YHGXBVSY-C8o3PcOo.js";import"./chunk-AZZRMDJM-DOUjzLYh.js";import"./chunk-RCXAM2GK-DPkcXkcJ.js";import"./chunk-VTHDQ7BV-BJJGfez0.js";import{m as e}from"./mermaid.esm.min-U7CnX3ab.js";import"./chunk-EICP7K5X-CPzr4HXR.js";import"./app-BsECO0k0.js";var a=e(r=>`${o(r)}
+  .swimlane.cluster rect {
+    stroke: ${r.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,u=t({defaultLayout:"swimlane",styles:m});export{u as diagram};
